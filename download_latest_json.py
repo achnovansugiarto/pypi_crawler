25e671204924cebb819f185620d68eef63ff6f57
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-27 08:45:00 +0800

    feature: get json downloader for latest

---
+++ download_latest_json.py
@@ -0,0 +1,15 @@
+import sys
+import requests
+import json
+
+def get_data(pkg_name):
+    url = f'https://pypi.org/pypi/{pkg_name}/json'
+    result = requests.get(url).json()
+    return result
+
+if __name__ == '__main__':
+    pkg_name = sys.argv[1]
+    result = get_data(pkg_name)
+    rc = len(result['releases'].keys())
+    with open(f'data/latest/{pkg_name}.rc{rc}.json', 'w', encoding='utf8') as f:
+        json.dump(result, f, ensure_ascii=False)
