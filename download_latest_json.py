1a546ba690ea91e95bde72fe288ab6549d5b9dfd
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-27 08:46:42 +0800

    fix: format

--- download_latest_json.py
+++ download_latest_json.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     result = get_data(pkg_name)
     rc = len(result['releases'].keys())
     with open(f'data/latest/{pkg_name}.rc{rc}.json', 'w', encoding='utf8') as f:
-        json.dump(result, f, ensure_ascii=False)
+        json.dump(result, f, ensure_ascii=False, indent=2)
